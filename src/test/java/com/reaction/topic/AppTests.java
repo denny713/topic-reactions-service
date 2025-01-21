68555c34727e942a1b4d8d2ecfd4eba9e071c964
Denny Afrizal <denny.afrizal713@gmail.com>
2025-01-21 18:04:58 +0700

    Project Skeleton

---
+++ src/test/java/com/reaction/topic/AppTests.java
@@ -0,0 +1,13 @@
+package com.reaction.topic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
