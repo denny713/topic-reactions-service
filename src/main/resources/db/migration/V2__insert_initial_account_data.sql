f383ed51c6cff3e24ec3b246949392b23aaa792f
Denny Afrizal <denny.afrizal713@gmail.com>
2025-01-22 00:50:31 +0700

    Fix Flyway Problem and Create Account Table

---
+++ src/main/resources/db/migration/V2__insert_initial_account_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO account (email, full_name, password, created_by, created_date)
+VALUES ('admin@mail.com', 'Administrator', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8',
+        'System', CURRENT_TIMESTAMP);
